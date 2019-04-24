32576cb6ff429658afe0a517069260b38787317d
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-04-24 03:58:31 +0300

    tehty lisää

--- SmartHome/SmartHome/MainPage.xaml.cs
+++ SmartHome/SmartHome/MainPage.xaml.cs
@@ -10,8 +10,11 @@ namespace SmartHome
 {
     public partial class MainPage : TabbedPage
     {
-        String paalle = "Hälytys on kytketty päälle!";
-        String pois = "Hälytys on pois päältä.";
+        String halypaalle = "Hälytys on kytketty päälle!";
+        String halypois = "Hälytys on pois päältä.";
+
+        string kiuasPaalle = "Kiuas on päällä.";
+        string kiuasPois = "Kiuas on sammutettu.";
 
         public MainPage()
         {
@@ -20,7 +23,7 @@ namespace SmartHome
             Slider2.Minimum = 0;
             Slider3.Minimum = 0;
             Slider4.Minimum = 0;
-
+            
             Slider1.Maximum = 10;
             Slider2.Maximum = 10;
             Slider3.Maximum = 10;
@@ -31,15 +34,28 @@ namespace SmartHome
               private void HalytysSwitch_PropertyChanging(object sender, PropertyChangingEventArgs e)
         {
                 if (HalytysSwitch.IsToggled == false)
-                //En tajua miksi se toimii näin päin, mutta näin se toimii.
+                //Jännä että se toimii näin päin, mutta näin se toimii.
                 //Eli kun togggled on false sanotaan hälytys on kytketty päälle.
                 {
-                Halytys_Label.Text = paalle;
+                Halytys_Label.Text = halypaalle;
                 }
                 else
                 {
-                Halytys_Label.Text = pois;
+                Halytys_Label.Text = halypois;
                 }
+        }
+
+        private void Sauna_Switch_PropertyChanging(object sender, PropertyChangingEventArgs e)
+        {
+            if (Sauna_Switch.IsToggled == false)
+            
+            {
+                Sauna_Label.Text = kiuasPaalle;
+            }
+            else
+            {
+                Sauna_Label.Text = kiuasPois;
+            }
         }
     }
 }
