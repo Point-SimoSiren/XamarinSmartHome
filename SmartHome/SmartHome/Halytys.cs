ee3542d9736d241715afeef2483e4c083ed22832
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-04-26 06:34:04 +0300

    Lisätty halytysluokka xamarin.forms projektiin

---
+++ SmartHome/SmartHome/Halytys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHome
+{
+    class Halytys
+    {
+        public int HalytinId { get; set; }
+        public int HalytinStatus { get; set; }
+    }
+}
